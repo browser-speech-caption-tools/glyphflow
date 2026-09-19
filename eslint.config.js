356d406d944b67ac7be663f304348a3261425f9f
enumura1 <akirayuto0718@gmail.com>
2026-09-20 01:30:08 +0900

    chore: ignore generated documentation files in lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,14 @@ import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
 
 export default [
-  { ignores: ["dist", "node_modules"] },
+  {
+    ignores: [
+      "dist",
+      "node_modules",
+      "docs-site/build",
+      "docs-site/.docusaurus",
+    ],
+  },
   js.configs.recommended,
   ...tseslint.configs.recommended,
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
